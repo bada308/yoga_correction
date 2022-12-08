1fdde1f7b954fe7a9bca95eec3e004ae43599651
bada308 <gangbada890@gmail.com>
2022-12-08 18:38:03 +0900

    fix : 요가 수행 페이지에서 요가 동작 교체 되도록 수정

--- frontend/src/utils/helper/index.js
+++ frontend/src/utils/helper/index.js
@@ -1,16 +1,15 @@
-
 export function drawSegment(ctx, [mx, my], [tx, ty], color) {
-    ctx.beginPath()
-    ctx.moveTo(mx, my)
-    ctx.lineTo(tx, ty)
-    ctx.lineWidth = 5
-    ctx.strokeStyle = color
-    ctx.stroke()
+  ctx.beginPath();
+  ctx.moveTo(mx * 1.5, my * 1.5);
+  ctx.lineTo(tx * 1.5, ty * 1.5);
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = color;
+  ctx.stroke();
 }
 
 export function drawPoint(ctx, x, y, r, color) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.fillStyle = color;
-    ctx.fill();
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
 }
